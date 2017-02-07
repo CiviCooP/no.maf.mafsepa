41381cc25b0ec03a7c0a18ba3651c8f1b3a713e7
Jaap Jansma <jaap@edeveloper.nl>
2017-02-07 13:15:07 +0100

    Fixed issue #988

---
+++ CRM/Mafsepa/Upgrader.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Collection of upgrade steps.
+ */
+class CRM_Mafsepa_Upgrader extends CRM_Mafsepa_Upgrader_Base {
+
+  protected $wordReplacements = array(
+    array(
+      'original' => 'SEPA',
+      'replacement' => 'Avtale Giro',
+      'exactMatch' => false,
+    ),
+    array(
+      'original' => 'CiviSEPA Dashboard',
+      'replacement' => 'Avtale Giro Dashboard',
+      'exactMatch' => true,
+    ),
+  );
+
+  public function postInstall() {
+    foreach($this->wordReplacements as $replacement) {
+      $params = array();
+      $params['find_word'] = $replacement['original'];
+      $params['replace_word'] = $replacement['replacement'];
+      if (!empty($replacement['exactMatch'])) {
+        $params['match_type'] = "exactMatch";
+      }
+      civicrm_api3('WordReplacement', 'create', $params);
+    }
+  }
+
+  public function uninstall() {
+    foreach($this->wordReplacements as $replacement) {
+      try {
+        $params = array();
+        $params['find_word'] = $replacement['original'];
+        $params['replace_word'] = $replacement['replacement'];
+        $word_replacement = civicrm_api3('WordReplacement', 'getsingle', $params);
+        civicrm_api3('WordReplacement', 'delete', array('id' => $word_replacement['id']));
+      } catch (Exception $e) {
+        // Do nothing;
+      }
+    }
+  }
+
+  public function enable() {
+    foreach($this->wordReplacements as $replacement) {
+      try {
+        $params = array();
+        $params['find_word'] = $replacement['original'];
+        $params['replace_word'] = $replacement['replacement'];
+        $word_replacement = civicrm_api3('WordReplacement', 'getsingle', $params);
+        $word_replacement['is_active'] = '1';
+        civicrm_api3('WordReplacement', 'create', $word_replacement);
+      } catch (Exception $e) {
+        // Do nothing;
+      }
+    }
+  }
+
+  public function disable() {
+    foreach($this->wordReplacements as $replacement) {
+      try {
+        $params = array();
+        $params['find_word'] = $replacement['original'];
+        $params['replace_word'] = $replacement['replacement'];
+        $word_replacement = civicrm_api3('WordReplacement', 'getsingle', $params);
+        $word_replacement['is_active'] = '0';
+        civicrm_api3('WordReplacement', 'create', $word_replacement);
+      } catch (Exception $e) {
+        // Do nothing;
+      }
+    }
+  }
+
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # no.maf.mafsepa
 
 This extension contains customisations for the Civi Sepa extensions for MAF Norway.
+
+## Word Replacements
+
+When you enable this extension those word replacements are automatically added:
+
+* Replace **CiviSepa Dashboard** with **Avtale Giro Dashboard**
+* Replace **SEPA** with **Avtale Giro**
